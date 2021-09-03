515a74dc2d3b0b592d43871cb3f995ad57e6d2e1
huajip <duhengzi@gmail.com>
2021-09-04 01:29:39 +0800

    添加khn图

--- pohai/convertjpg2png.py
+++ pohai/convertjpg2png.py
@@ -5,5 +5,5 @@ import re
 for file in glob.glob('*.jpg'):
     file = re.sub('.jpg','', file)
     file2 = file[:-4]
-    os.system('ffmpeg -i ' + file + '.jpg ' + file + '.png')
-    os.system('del ' + file + '.jpg')
+    os.system('ffmpeg -y -i ' + file + '.jpg ' + file + '.png')
+    os.system('rm -rf ' + file + '.jpg')
